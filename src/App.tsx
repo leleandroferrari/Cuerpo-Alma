c6309a3abc44e5f03faa19c66fdc945f92ec6c9d
Leandro Ferrari <leleandroferrari@users.noreply.github.com>
2026-03-19 14:49:52 +0100

    Apply definitive business card style guide (Navy & Gold)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ColorSwitcher from "./components/ColorSwitcher";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Impressum from "./pages/Impressum";
@@ -16,7 +15,6 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <ColorSwitcher />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
